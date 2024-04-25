22a4c73791b16eb125f70108fefe36ddf18eedf0
hwcall1102 <haydenc0293@gmail.com>
2024-04-24 22:53:51 -0600

    Prep2 Complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?  ");
+        string gPercent = Console.ReadLine();
+        int gNumber = int.Parse(gPercent);
+
+        string letter = "";
+        
+        if (gNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string sign = "";
+
+        if (gNumber % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (gNumber % 10 < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (gNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You Passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time.");
+        }
     }
 }
