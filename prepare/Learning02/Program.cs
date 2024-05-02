addd2ed2acf437e70e389d8c330c17d71abf2c4e
hwcall1102 <haydenc0293@gmail.com>
2024-05-01 21:19:43 -0600

    Abstraction Resume prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Portfolio Manager";
+        job1._company = "Clicklease";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "English 12 Teacher";
+        job2._company = "Farmington High School";
+        job2._startYear = 2019;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Hayden Call";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
