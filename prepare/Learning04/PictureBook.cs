17a8942c7d044c77c21f8fd0af9c61d886c4402a
hwcall1102 <haydenc0293@gmail.com>
2024-05-15 23:09:28 -0600

    Week4 learning

---
+++ prepare/Learning04/PictureBook.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace library_demo
+{
+    public class PictureBook : Book
+    {
+        private string _illustrator = "";
+
+        public PictureBook() : base()
+        {
+            
+        }
+
+        public PictureBook(string author, string title, string illustrator) : base(author, title)
+        {
+            _illustrator = illustrator;
+        }
+
+        public string GetIllustrator()
+        {
+            return _illustrator;
+        }
+
+        public void SetIllustrator(string illustrator)
+        {
+            _illustrator = illustrator;
+        }
+
+        public string GetPictureBookInfo()
+        {
+            return $"{GetTitle()} by {GetAuthor()} illustrated by {_illustrator}";
+        }
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using homework;
+using library_demo;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment test1 = new Assignment("Samuel Bennett", "Multiplication");
+
+        Console.WriteLine(test1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment write1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(write1.GetSummary());
+        Console.WriteLine(write1.GetWritingInformation());
+
+        /*Book book1 = new Book();
+        book1.SetAuthor("Smith");
+        book1.SetTitle("A Great Book");
+
+        Console.WriteLine(book1.GetBookInfo());
+
+        PictureBook book2 = new PictureBook();
+        book2.SetAuthor("Anderson");
+        book2.SetTitle("The Little Mermaid");
+        book2.SetIllustrator("Hayden Call");
+
+        Console.WriteLine(book2.GetPictureBookInfo());
+
+        Book book3 = new Book("Edwards", "new book");
+        Console.WriteLine(book3.GetBookInfo());
+
+        PictureBook book4 = new PictureBook("Suess", "Oh the PLaces You'll Go", "Suess");
+        Console.WriteLine(book4.GetPictureBookInfo());*/
+
+
+
+
     }
 }
